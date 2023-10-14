61e716d9bab891195bd7fa306b717b4db87768c4
Carlo <carloarturomerinoc@gmail.com>
2023-10-14 11:59:09 -0300

    Quitadas funciones innecesarias
    y añadida función previa a escribir e un RTree en memoria

--- Nearest-X/NearestXRtree.cpp
+++ Nearest-X/NearestXRtree.cpp
@@ -15,65 +15,6 @@ using namespace std;
 
 int l=0;
 
-
-/* función auxiliar que genera un vector de rectángulos en base a N puntos */
-vector <Rectangulo> makeRectArray(d_int arr[], d_int N){
-    int L = N >> 2;
-    vector<Rectangulo>vec(L, Rectangulo(Punto(0,0),Punto(0,0)));
-    for (int i = 0; i  <L; i++){
-        d_int x1 =arr[i*4];
-        d_int y1 =arr[i*4+1];
-        d_int x2 =arr[i*4+2];
-        d_int y2 =arr[i*4+3];
-        Rectangulo rect = Rectangulo(Punto(x1,y1),Punto(x2,y2)); 
-        vec[i] = rect;
-    }
-   return vec;
-}
-
- /*algoritmo quicksort para ordenar un rectángulos de menor a mayor respecto a la abscisa*/
-void xquickSort(vector <Rectangulo> &arr, int start, int end){ 
- 
-    //caso base
-    if (start >= end)
-        return;
-
-    // se particiona el arreglo
-    d_int pivot = arr[start].centro.x;
-    d_int count = 0;
-    for (int i = start + 1; i <= end; i++) {
-        if (arr[i].centro.x <= pivot)
-            count++;
-    }
-    // asignando una posición al pivote
-    int pivotIndex = start + count;
-
-
-    swap(arr[pivotIndex], arr[start]);
-
- 
-    // ordenando particiones izquierdas y derechas del pivote
-    int i = start, j = end;
- 
-    while (i < pivotIndex && j > pivotIndex) {
- 
-        while (arr[i].centro.x <= pivot) {
-            i++;
-        }
- 
-        while (arr[j].centro.x > pivot) {
-            j--;
-        }
- 
-        if (i < pivotIndex && j > pivotIndex) {
-            swap(arr[i++], arr[j--]);
-        }
-    }
-    // se ordena la mitad izquierda
-    xquickSort(arr, start, pivotIndex - 1);
-    // Sorting the right part
-    xquickSort(arr, pivotIndex + 1, end);
-}
 /*Función auxiliar que encuentra un MBR en el rango [start,end] de una lista de rectangulos*/
 Rectangulo calcularMBR(vector<Rectangulo>& listaRectangulos,int start, int end) {
     int lsize = listaRectangulos.size();
@@ -102,82 +43,114 @@ Rectangulo calcularMBR(vector<Rectangulo>& listaRectangulos,int start, int end) 
     // retornamos el MBR calculado como un nuevo rectángulo
     return Rectangulo(Punto(minX, minY), Punto(maxX, maxY));
 }
-/* Función que construye un grupo de nodos hoja en base a una lista de rectángulos*/
-vector <NodoRTree> makeLeafs(vector<Rectangulo> &rects){
-    //tamaño del vector
-    int n = sizeof(rects)/sizeof(rects[0]);
-    //se crea el vector de nodos
-    vector<NodoRTree> nodos(n,NodoRTree(true,1));
-    int i = 0;
-    for(Rectangulo r: rects){
-        nodos[i].insertarRectangulo(r);
-        i++;
-    }
-    return nodos;
-} 
+
 /*
-Función que  genera un único nodo raíz en base a una lista de rectángulos. 
-Recibe:
-  r_vect:  un vector de rectángulos, m: la cantidad máxima de nodos 
-Entrega: 
-   un  NodoRTree 
+Función que  genera un array que linealmente representa a un  RTree
+La estructura final se disponde de la siguiente forma en memoria:
+
+0  rectangulos     R    claves   3k   k
+|------------------|----|----|----|----|
+|__________________|____ ___________h__|....
+                    
+---cada rectángulo contiene 4 enteros: 4*R enteros
+---cada nodo contiene 4k + k enteros; 
+            4k para rectangulos  MBR
+            k  para punteros (offset) a hijos.
+--- se tienen R/m nodos en total
+
+--- el offset unitario que más sentido tiene usar es sizeof(int) por simplicidad
+
 */
-NodoRTree nX_RTree(vector <Rectangulo> &r_vect, int m){
-    // creación del nodo a retornar
-    NodoRTree Rtree = NodoRTree(false,m);
-    // se calcula la cantidad total  de rectángulos  a calcular
+int offset=sizeof(int);
+void nX_RTree(vector <Rectangulo> &r_vect, int m){
+    //vector que contendrá los datos a insertar en la interación actual
+    vector <int> arr(1,0);
+    // se calcula la cantidad de rectángulos totales
     int R_totales = sizeof(r_vect) / sizeof(r_vect[0]);
     //Se ordenan los rectángulos del vector de entrada
     xquickSort(r_vect,0,R_totales - 1);
-    int i = 0;
+    //se escribe r_vect en memoria como un nodo hijo (anteponiendo un offset a nulo)
+    for (Rectangulo r : r_vect){
+        arr.push_back(0);
+        arr.push_back(r.p1.x);
+        arr.push_back(r.p1.y);
+        arr.push_back(r.p2.x);
+        arr.push_back(r.p2.y);
+    }//con esto, se tienen R rectángulos compuestos por 5 enteros.
 
-    // Si la cantidad de rectángulos total cabe en el nodo, se añaden los rectángulos como nodos hoja
-    if (R_totales<=m){
-        vector <NodoRTree> hijos = makeLeafs(r_vect);
-        for (NodoRTree h : hijos){
-            Rtree.insertarHijo(h);
-            Rtree.insertarRectangulo(r_vect[i]);
-            i++;
-        }
+    //se calcula la cantidad de mbrs a calcular
+    //Notar que esto también corresponde a cuantos nodos se crearán en esta iteración
+    int n_mbr = R_totales/m;
+
+    // se calculan los mbrs
+    vector <Rectangulo> mbrs(n_mbr,Rectangulo(Punto(0,0),Punto(0,0)));
+    // Se recorre el vector de rectángulos para generar los MBR
+    for (int rect = 0; rect < n_mbr; rect++){  
+        mbrs[rect] = calcularMBR(r_vect,rect*m,rect*(m+1)); 
     }
-    else{
-        /* se calcula la cantidad de iteraciones que serán necesarias para a los rectangulos.
-           O iteraciones = lo del if:  
-                Se tiene un único nodo no hoja que contiene a lo  más m rectángulos y m punteros a 
-                nodos hoja que  contienen cada uno uno de los rectángulos anteriores.
-             
-           1 iteración = bastan nr<=m MBRs para cubrir todos los rectángulos:
-              => el nodo Rtree contiene nr MBRs y nr punteros a los nodos, los cuales cumplen lo 
-              anterior
-            ... etc ...
-        */
-        int rt = R_totales;
-        while(rt > m){
-            rt = rt/m;
-            i++;
-        }
-        // se crea un vector de punteros de arreglos que contendrán los resultados de cada iteración
-        vector <NodoRTree**> ptrs[3];
-        //se calcula el tamaño de la partición de  rectángulos
-        int part = (R_totales/m);
-        for (rt; rt>0; rt--){
-            vector<NodoRTree**> 
-            for(){
 
-            }
-                ptrs->insert(part,NodoRTree(false,m));
-            // Se genera un vector de  MBR  rectángulos para el grupo de m rectángulos, 
-            vector <Rectangulo> mbrs(R_totales/m,Rectangulo(Punto(0,0),Punto(0,0)));
-            int counter = 0;
-            for (int rect = 0; rect < R_totales/m; rect++){  
-                mbrs[rect] = calcularMBR(r_vect,counter,counter+m);
-                }
-                counter+=m;
-                nodos->
-            }
+    // se setablece la posición del primer nodo hijo
+    int offset = 1;
+    //Se van insertando los nodos padre de a m datos
+    for (int padres = 0; padres < n_mbr; padres++){
+        int ds = m*padres;              //desplazamiento para los mbrs  
+        if (ds > n_mbr) ds = n_mbr;     // si ds > n_mbr pasan cosas feas :s
+        for (int k = 0; k<m; k++){      //primero los "punteros" a hijos
+            arr.push_back(offset);
+            offset +=5;                 //se avanza a la primera posición de la siguiente hoja
+        }
+        for (int k = 0; k<m; k++){      //luego las claves (rectángulos mbr)
+            arr.push_back(mbrs[k+ds].p1.x);
+            arr.push_back(mbrs[k+ds].p1.y);
+            arr.push_back(mbrs[k+ds].p2.x);
+            arr.push_back(mbrs[k+ds].p2.y);
             
         }
     }
+    // si la cantidad de mbrs fué mayor a la cantidad de datos guardables en un nodo,
+    // se repite una variación del algoritmo anterior.
+    if (n_mbr> m){
+        int data = n_mbr;
+        /* se crean vectores para contener los mbrs anteriores y a los actuales. 
+          Notar que el tamañano efectivamente utilizado nunca puede ser mayor a n_mbr original */
+        vector <Rectangulo> ans_mbrs = mbrs;
+        vector <Rectangulo> new_mbrs(n_mbr/m,Rectangulo(Punto(0,0),Punto(0,0)));
 
-return Rtree;
+
+        while (data > n_mbr){
+            /*se (re)calcula la cantidad de mbrs a calcular. Notar que esto también corresponde a 
+             cuantos nodos se crearán en esta iteración, y hasta que nivel se usarán los vectores
+             anteriores */
+            int nmbr = data/m;
+            // Se recorre el vector de rectángulos para generar los MBR
+            for (int rect = 0; rect < nmbr; rect++){  
+                new_mbrs[rect] = calcularMBR(ans_mbrs,rect*m,rect*(m+1)); 
+            }
+            //Se van insertando datos a los  nodos ascendentes de a m datos (hasta llenar los)
+            for (int padres = 0; padres < nmbr; padres++){
+                for (int k = 0; k<m; k++){      //primero los "punteros" a hijos
+                    arr.push_back(offset);
+                    offset +=5*m;               //se avanza a la primera posición del siguiente nodo
+                }
+                int K = m;                      //cantidad de datos a insertar 
+                if (nmbr < n_mbr) K = n_mbr;    // si ya se ha logrado que data = nmbr < n_mbrm, K = nmbr
+                for (int k = 0; k<K; k++){      //luego las claves (rectángulos mbr)
+                    arr.push_back(new_mbrs[k].p1.x);
+                    arr.push_back(new_mbrs[k].p1.y);
+                    arr.push_back(new_mbrs[k].p2.x);
+                    arr.push_back(new_mbrs[k].p2.y);
+                    
+                } 
+            }
+            // ahora se new_mbr debe ser ans_mbr
+            for (int i = 0; i<nmbr; i++){
+                ans_mbrs[i]=new_mbrs[i];
+            }
+            // Se actualiza el valor de data
+            data = data/m;
+
+        }
+    }
+    // Se escribe el RTree en un binario
+    
 }
--- Nearest-X/nodoRTree-struct.h
+++ Nearest-X/nodoRTree-struct.h
@@ -39,66 +39,61 @@ struct Rectangulo{
     Punto centro;
     Rectangulo(Punto p1, Punto p2) : p1(p1), p2(p2),  centro(calcularPuntoMedio(p1,p2)) {}
 };
-
-/* Clase para representar un nodoRtree: contiene una lista de claves (rectángulos) y una lista de hijos(nodos) */
-class NodoRTree{
-    public:
-    /* Constructor para un nodo interno */
-    NodoRTree(bool esHoja, int limite) : esHoja_(esHoja), m(limite) {}
-    
-    /* Destructor  para liberar la memoria de todos los nodos hijos*/
-    ~NodoRTree();
-    /* función para insertar un rectángulo en el nodo */
-    bool insertarRectangulo(const Rectangulo &rectangulo);
-    void insertarHijo(const NodoRTree hijo);
-    /*Función para rescatar los rectángulos de un nodo*/
-    void *rescatarRectangulos();
-    private:
-        bool esHoja_;
-        /* cada nodo contiene k rectángulos de los MBR de sus hijos*/
-        vector<Rectangulo> rectangulos_;
-        /* cada nodo contiene k punteros a los hijos de los MBR anteriores*/
-        vector<NodoRTree *> hijos_;
-        bool estaLleno() const;
-        int m;
-};
-
-// OPERADORES CLASE ==============================================================
-/* función para liberar memoria de los hijos de un nodo*/
-NodoRTree::~NodoRTree(){
-    // Liberar la memoria de los nodos hijos (si es necesario)
-    for (void *hijo : hijos_){
-        delete hijo;
+/* función auxiliar que genera un vector de rectángulos en base a N puntos */
+vector <Rectangulo> makeRectArray(d_int arr[], d_int N){
+    int L = N >> 2;
+    vector<Rectangulo>vec(L, Rectangulo(Punto(0,0),Punto(0,0)));
+    for (int i = 0; i  <L; i++){
+        d_int x1 =arr[i*4];
+        d_int y1 =arr[i*4+1];
+        d_int x2 =arr[i*4+2];
+        d_int y2 =arr[i*4+3];
+        Rectangulo rect = Rectangulo(Punto(x1,y1),Punto(x2,y2)); 
+        vec[i] = rect;
     }
+   return vec;
 }
-/* Función para insertar hijos en un nodo */
-void NodoRTree::insertarHijo(NodoRTree hijo){
-    hijos_.push_back(&hijo);
 
-}
-/* Función para rescatar los rectángulos de un nodo */
-void* NodoRTree::rescatarRectangulos(){
-    if (esHoja_){
-        return (&rectangulos_[0]);
+ /*algoritmo quicksort para ordenar un vector rectángulos de menor a mayor respecto a la abscisa*/
+void xquickSort(vector <Rectangulo> &arr, int start, int end){ 
+ 
+    //caso base
+    if (start >= end)
+        return;
+
+    // se particiona el arreglo
+    d_int pivot = arr[start].centro.x;
+    d_int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i].centro.x <= pivot)
+            count++;
     }
-    return &rectangulos_;
-}
-/* Función para insertar rectángulos en un nodo */
-bool NodoRTree::insertarRectangulo(const Rectangulo &rectangulo)
-{
-    // Verificar si el nodo está lleno
-    if (estaLleno()){
-        return false; // Indica que no se pudo insertar el rectángulo
+    // asignando una posición al pivote
+    int pivotIndex = start + count;
+
+
+    swap(arr[pivotIndex], arr[start]);
+
+ 
+    // ordenando particiones izquierdas y derechas del pivote
+    int i = start, j = end;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (arr[i].centro.x <= pivot) {
+            i++;
+        }
+ 
+        while (arr[j].centro.x > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
     }
-
-    // Si el nodo no está lleno, simplemente agregamos el rectángulo
-    rectangulos_.push_back(rectangulo);
-    return true; // Indica que el rectángulo se insertó correctamente
-
+    // se ordena la mitad izquierda
+    xquickSort(arr, start, pivotIndex - 1);
+    // Sorting the right part
+    xquickSort(arr, pivotIndex + 1, end);
 }
-
-bool NodoRTree::estaLleno() const{
-    return static_cast<int>(rectangulos_.size()) >= m; // m es el límite máximo de rectángulos
-}
-
-
