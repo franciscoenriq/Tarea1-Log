7c5becde4ab06367b625d7d9ef74a2fc9df1d480
Carlo <carloarturomerinoc@gmail.com>
2023-10-14 17:40:11 -0300

    Refactory
    descartado struct.h

--- Nearest-X/NearestXRtree.cpp
+++ Nearest-X/NearestXRtree.cpp
@@ -10,12 +10,71 @@
 #include <cstdint>
 #include <cstdlib>
 #include "bits/stdc++.h"
-#include "nodoRTree-struct.h"
+#include "../NodoRTree.cpp"
 #include "valarray"
+#define p1 inf_izq
+#define p2 sup_der
+#define d_int int
 using namespace std;
 
-int l=0;
+/* función auxiliar que genera un vector de rectángulos en base a N puntos */
+vector <Rectangulo> makeRectArray(d_int arr[], d_int N){
+    int L = N >> 2;
+    vector<Rectangulo>vec(L, Rectangulo(Punto(0,0),Punto(0,0)));
+    for (int i = 0; i  <L; i++){
+        d_int x1 =arr[i*4];
+        d_int y1 =arr[i*4+1];
+        d_int x2 =arr[i*4+2];
+        d_int y2 =arr[i*4+3];
+        Rectangulo rect = Rectangulo(Punto(x1,y1),Punto(x2,y2)); 
+        vec[i] = rect;
+    }
+   return vec;
+}
 
+ /*algoritmo quicksort para ordenar un vector rectángulos de menor a mayor respecto a la abscisa*/
+void xquickSort(vector <Rectangulo> &arr, int start, int end){ 
+ 
+    //caso base
+    if (start >= end)
+        return;
+
+    // se particiona el arreglo
+    d_int pivot = arr[start].centro.x;
+    d_int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i].centro.x <= pivot)
+            count++;
+    }
+    // asignando una posición al pivote
+    int pivotIndex = start + count;
+
+
+    swap(arr[pivotIndex], arr[start]);
+
+ 
+    // ordenando particiones izquierdas y derechas del pivote
+    int i = start, j = end;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (arr[i].centro.x <= pivot) {
+            i++;
+        }
+ 
+        while (arr[j].centro.x > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+    // se ordena la mitad izquierda
+    xquickSort(arr, start, pivotIndex - 1);
+    // Sorting the right part
+    xquickSort(arr, pivotIndex + 1, end);
+}
 /*Función auxiliar que encuentra un MBR en el rango [start,end] de una lista de rectangulos*/
 Rectangulo calcularMBR(vector<Rectangulo>& listaRectangulos,int start, int end) {
     int lsize = listaRectangulos.size();
